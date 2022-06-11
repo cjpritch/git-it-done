63aaf77e02fa0930e7c18dce6536e23224e9c7d4
Carly Pritchard <cpritch616@gmail.com>
2022-06-11 16:43:04 -0500

    add link if more than 30 issues in repo

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,5 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
 
 // function to fetch info from github api
 var getRepoIssues = function(repo) {
@@ -14,6 +15,10 @@ var getRepoIssues = function(repo) {
          // pass response data to dom function
         displayIssues(data);
           });
+        }
+        // check if api has paginated issues
+        if (response.headers.get("Link")) {
+            displayWarning(repo);
         }
         else {
           alert("There was a problem with your request!");
@@ -61,5 +66,21 @@ var displayIssues = function(issues) {
     issueContainerEl.appendChild(issueEl);
         }
 };
-  // getRepoIssues("facebook/react");
+
+// if repo has more than 30 issues this will execute 
+var displayWarning = function(repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+  // append to warning container
+  limitWarningEl.appendChild(linkEl);
+  };
+
+
+  getRepoIssues("angular/angular");
   
